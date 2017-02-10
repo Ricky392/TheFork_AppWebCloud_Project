be6edfa9221623bb6527797b09cf297e33d83aa3
Riccardo Pagano <r.pagano392@gmail.com>
2017-02-10 14:25:11 +0100

    migliorata funzione hashcode

--- js/Utils.js
+++ js/Utils.js
@@ -4,13 +4,11 @@
 
 
 //hashcode functions for strings --- wll be used for crypting password during login / registration
-hashCode = function(str){
+function hashCode(str){
     var hash = 0;
     if (str.length == 0) return hash;
     for (i = 0; i < str.length; i++) {
-        char = str.charCodeAt(i);
-        hash = ((hash<<5)-hash)+char;
-        hash = hash & hash; // Convert to 32bit integer
+        char = str.charCodeAt(i);		hash = ((hash<<5)-hash)+char;		hash = hash & hash; // Convert to 32bit integer
     }
     return hash;
 }
